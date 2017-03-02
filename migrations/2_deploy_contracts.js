6896d3ba3d88328c02c10234bffc2ed4a1acb628
Mark Wanders <mndmwanders@gmail.com>
2017-03-02 11:44:08 +0100

    updated migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+var Project = artifacts.require("./Project.sol");
+var FundingHub = artifacts.require("./FundingHub.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.deploy(Project);
+  deployer.link(Project, FundingHub);
+  deployer.deploy(FundingHub);
 };
